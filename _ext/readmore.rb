0bb0dcbef83dfb4585075d324f13245538a2f3e1
Alexander Heusingfeld <ahe@ahembp.fritz.box>
2013-08-12 00:56:37 +0200

    fixed 'unclosed <p> element' bug
    fixed bug which made markup invalid due an unclosed <p> element. This occurred in post listing (index) when readmore.rb truncated the content.

--- _ext/readmore.rb
+++ _ext/readmore.rb
@@ -1,21 +1,22 @@
 module Awestruct
   module Extensions
     module ReadMore
-      def truncate(content, title, url)
+      def truncate(content)
         index = content.index("pass::[more]")
-        if index != nil
-            if index > -1
-                return [content[0..index-1], '<a href="', site.base_url, url, '" class="continue">...continue reading "', title, '"</a>']
-            end
+        if index != nil && index > -1
+          # check for additional opened and unclosed <p> tag
+          if (content[index-3..index-1].casecmp "<p>") == 0
+            return content[0..index-4]
+          else
+            return content[0..index-1]
+          end
         end
         return content
       end
-      def filter(content, title, url)
+      def filter(content)
         index = content.index("pass::[more]")
-        if index != nil
-            if index > -1
-                content[index..index+11]= '<a id="continue" name="continue"></a>'
-            end
+        if index != nil && index > -1
+                content[index..index+11]= ''
         end
         content
       end
